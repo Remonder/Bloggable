7d4792c9a24bf7704aeb191f750b1cc1572c6061
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-10-17 22:27:54 +0200

    :sparkles: Send article titles in test route

--- Sources/Bloggable/Commands/RunServerCommand.swift
+++ Sources/Bloggable/Commands/RunServerCommand.swift
@@ -9,15 +9,30 @@ import Commandant
 import Foundation
 import Kitura
 import Result
+import SwiftKuery
+import SwiftKueryMySQL
 
 struct RunServerCommand : CommandProtocol {
     var verb = "runServer"
     var function = "Runs the server."
     
     public func run(_ options: NoOptions<String>) -> Result<(), String> {
+        let connection = MySQLConnection(user: "root", database: "blog")
+        connection.connect { print($0?.localizedDescription ?? "No Error") }
+        
         let router = Router()
         router.get("/") { _, response, next in
-            response.send("Hello, World!")
+            response.send("START\n")
+            let query = Select(from: Article.TableV0())
+            connection.execute(query: query) { queryResult in
+                if let resultSet = queryResult.asResultSet {
+                    for row in resultSet.rows {
+                        let array = row as Array
+                        response.send(array.dropFirst().first as! String + "\n")
+                    }
+                }
+            }
+            response.send("END\n")
             next()
         }
         
@@ -27,4 +42,3 @@ struct RunServerCommand : CommandProtocol {
         return .success(())
     }
 }
-
